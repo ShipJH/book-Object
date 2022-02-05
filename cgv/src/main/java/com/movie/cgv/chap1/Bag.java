df151922f85c4b31e7676e0fa2b8178339f49b52
shipjh <shipjh@218.38.137.28>
2022-02-05 23:23:04 +0900

    chapter 4 init

--- cgv/src/main/java/com/movie/cgv/chap1/Bag.java
+++ cgv/src/main/java/com/movie/cgv/chap1/Bag.java
@@ -1,38 +1,37 @@
 package com.movie.cgv.chap1;
 
+/**
+ * 가방 구현 클래
+ * @author shipjh
+ *
+ */
 public class Bag {
 
 	private Long amount;
 	private Invitation invitation;
 	private Ticket ticket;
 	
-	public Bag(Invitation invitation, Long amount) {
-		this.invitation = invitation;
-		this.amount = amount;
+
+	public Long hold(Ticket ticket) {
+		if (hasInvitation()) {
+			setTicket(ticket);
+			return 0L;
+		} else {
+			setTicket(ticket);
+			minusAmount(ticket.getFee());
+			return ticket.getFee();
+		}
 	}
 	
-	public Bag(Long amount) {
-		this(null, amount);
-	}
-	
-	
-	public boolean hasInvitation() {
+	private boolean hasInvitation() {
 		return invitation != null;
 	}
-	
-	public boolean hasTicket() {
-		return ticket != null;
-	}
 
-	public void setTicket(Ticket ticket) {
+	private void setTicket(Ticket ticket) {
 		this.ticket = ticket;
 	}
 	
-	public void minusAmount(Long amount) {
+	private void minusAmount(Long amount) {
 		this.amount -= amount;
-	}
-	
-	public void plusAmount(Long amount) {
-		this.amount += amount;
 	}
 }
--- cgv/src/main/java/com/movie/cgv/chap1/TicketOffice.java
+++ cgv/src/main/java/com/movie/cgv/chap1/TicketOffice.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 티켓 판매
+ * 티켓 판매소 
  * @author shipjh
  *
  */
@@ -14,20 +14,15 @@ public class TicketOffice {
 	private Long amount;
 	private List<Ticket> tickets = new ArrayList<>();
 	
-	public TicketOffice(Long amount, Ticket ...tickets) { 
-		this.amount = amount;
-		this.tickets.addAll(Arrays.asList(tickets));
+	public void sellTicketTo(Audience audience) {
+		plusAmount(audience.buy(getTicket()));
 	}
 	
-	public Ticket getTicket() {
+	private Ticket getTicket() {
 		return tickets.remove(0);
 	}
 	
-	public void minusAmount(Long amount) {
-		this.amount -= amount;
-	}
-	
-	public void plusAmount(Long amount) {
+	private void plusAmount(Long amount) {
 		this.amount += amount;
 	}
 }
